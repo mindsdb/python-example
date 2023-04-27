328152a7945b81b47c497e0f2fbd2039cca3bf34
andrew <elkin@raketa.im>
2023-04-27 19:17:39 +0300

    sample app

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Mindsdb python SDK example application
+
+English to French translation
+- uses Mindsdb SDK to connect Mindsdb cloud
+- for translation is used t5-base hugginface model created in Mindsdb cloud
---
+++ main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import streamlit as st
+import mindsdb_sdk
+
+LOGIN = 'andrey@mindsdb.com'
+PASSWORD = st.secrets["password"]
+
+
+st.write("## English to French translation")
+st.write("Mindsdb python SDK example application: translate from English to French using mindsdb with t5-base hugginface model")
+
+st.write("[Example source code](https://github.com/mindsdb/mindsdb)")
+st.write("[Mindsdb python SDK documentation](https://github.com/mindsdb/mindsdb)")
+
+
+@st.cache_resource
+def get_model():
+    loading = st.empty()
+    loading.markdown('## connecting to mindsdb cloud server...')
+
+    server = mindsdb_sdk.connect(login=LOGIN, password=PASSWORD)
+    project = server.get_project()
+    _model = project.get_model('hf_t5_en_fr')  # hf_t5_en_fr is the name of model in mindsdb
+
+    loading.markdown("")
+    return _model
+
+
+model = get_model()
+
+source = st.text_area(label="Input text:")
+
+button = st.button(label='Translate')
+
+translated = st.code("", language="markdown")
+
+if button:
+    translated.markdown('*translating...*')
+
+    # convert text to dataframe
+    df_in = pd.DataFrame([{'text': source}])
+    df_out = model.predict(df_in)
+
+    # get translated text from output dataframe
+    text = df_out.PRED[0]
+    translated.markdown(text)
